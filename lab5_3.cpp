2935132ca03575d109474a5acc0b7cc8ab5a75a6
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-08 17:18:39 +0700

    DONE!

--- lab5_3.cpp
+++ lab5_3.cpp
@@ -1,11 +1,17 @@
 #include <stdio.h>
 int main()
 {
-	int n, number = 1;
+	int n, number = 0;
 	scanf("%d", &n);
 	do
 	{
-		
+			number++;
+		if (number == 1 || number % 2 == 0 || number % 3 == 0 || number % 5 == 0)
+		{
+			n--;
+		}
 	}
 	while (n > 0);
+	printf("%d", number);
+	return 0;
 }
