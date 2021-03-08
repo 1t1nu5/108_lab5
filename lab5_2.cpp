874c6b61b6a61b8622399774b3870dc26c251046
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-08 16:46:23 +0700

    add middle case

--- lab5_2.cpp
+++ lab5_2.cpp
@@ -5,6 +5,16 @@ int main()
 	scanf("%d", &n);
 	do
 	{
+		if (n%2 == 1 && line == (n-1)/2)
+		{
+			printf("*");
+			for (int i = 0; i < n - 1; i++)
+			{
+				printf("-");
+			}
+			printf("*");
+		}
+		else if (line == n/2)
 		
 	} 
 	while (line < n);
