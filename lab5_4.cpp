2d44b8c3ae88416bbaaee4b542acaa21528134f7
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-08 17:45:23 +0700

    add more variable and receiver

--- lab5_4.cpp
+++ lab5_4.cpp
@@ -2,8 +2,11 @@
 #include <string.h>
 int main()
 {
-	char nom[17], op[17], result[17];
+	char nom[17], op[17], result[17], most;
 	int digit;
+	scanf("%s", nom);
+	scanf("%s", op);
+	scanf("%s", result);
 	return 0;
 	//16
 }
