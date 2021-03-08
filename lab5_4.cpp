e4abd820b7daf2ec4b888a308c59a4db36385bca
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-08 17:46:38 +0700

    most value character finder

--- lab5_4.cpp
+++ lab5_4.cpp
@@ -2,11 +2,18 @@
 #include <string.h>
 int main()
 {
-	char nom[17], op[17], result[17], most;
+	char nom[17], op[17], result[17], most = NULL;
 	int digit;
 	scanf("%s", nom);
 	scanf("%s", op);
 	scanf("%s", result);
+	for (int i = 0; i < 17; i++)
+	{
+		if (most < nom[i])
+		{
+			most = nom[i];
+		}
+	}
 	return 0;
 	//16
 }
