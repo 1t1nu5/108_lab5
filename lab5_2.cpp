f2d5cd401735331da64bf0920f516235f23c84f1
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-08 18:52:31 +0700

    set default value

--- lab5_2.cpp
+++ lab5_2.cpp
@@ -4,6 +4,36 @@ int main()
 {
 	int maxline, line, direction = false;
 	scanf("%d", maxline);
+	if (maxline % 2 == 0)
+	{
+		char print[maxline-1];
+		for (int i = 0; i < maxline-1; i++)
+		{
+			if (i = maxline/2-1)
+			{
+				print[i] = '*';
+			}
+			else
+			{
+				print[i] = '-';
+			}
+		}
+	}
+	else
+	{
+		char print[maxline];
+		for (int i = 0; i < maxline-1; i++)
+		{
+			if (i = maxline/2)
+			{
+				print[i] = '*';
+			}
+			else
+			{
+				print[i] = '-';
+			}
+		}
+	}
 	do
 	{
 		
