f28d7cfb373cd0aa2d4e7f6ea62eee1fb09b01ec
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-08 16:49:37 +0700

    add first and last case

--- lab5_2.cpp
+++ lab5_2.cpp
@@ -5,7 +5,7 @@ int main()
 	scanf("%d", &n);
 	do
 	{
-		if ((n%2 == 1 && line == (n-1)/2) || (n%2 == 0 && line == n/2 || line == n/2 - 1))
+		if ((n%2 == 1 && line == (n-1)/2) || (n%2 == 0 && line == n/2 || line == n/2-1))
 		{
 			printf("*");
 			for (int i = 0; i < n - 1; i++)
@@ -14,8 +14,19 @@ int main()
 			}
 			printf("*");
 		}
-		
-		
+		else if (line == 0 || line == n-1)
+		{
+			for (int i = 0; i < (n-1)/2; i++)
+			{
+				printf("-");
+			}
+			printf("*");
+			for (int i = 0; i < (n-1)/2; i++)
+			{
+				printf("-");
+			}
+		}
+		printf("\n");
 	} 
 	while (line < n);
 	return 0;
