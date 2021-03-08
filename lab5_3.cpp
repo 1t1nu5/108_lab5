0417f304b5d3b44590cf3f6033af9a7c13fc93f7
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-08 18:38:58 +0700

    try coding...

--- lab5_3.cpp
+++ lab5_3.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-	int n, number = 1;
+	int n, number = 2;
 	scanf("%d", &n);
 	if (n == 1)
 	{
@@ -10,7 +10,24 @@ int main()
 	}
 	do
 	{
-		
+		int i = number;
+		while (i % 2 == 0)
+		{
+			i /= 2;
+		}
+		while (i % 3 == 0)
+		{
+			i /= 3;
+		}
+		while (i % 5 == 0)
+		{
+			i /= 5;
+		}
+		if (i == 1)
+		{
+			n--;
+		}
+		number++;
 	}
 	while (n > 0);
 	printf("%d", number);
