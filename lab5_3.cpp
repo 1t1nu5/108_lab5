d4298aab10ac243829bda4dc4c54e7f539ff10c3
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-08 18:34:58 +0700

    clear and try again

--- lab5_3.cpp
+++ lab5_3.cpp
@@ -1,59 +1,11 @@
 #include <stdio.h>
-#include <stdbool.h>
 int main()
 {
-	int n, number = 1, cnumber;
-	bool check = false;
+	int n, number = 1
 	scanf("%d", &n);
 	do
 	{
-		printf("%d\t%d\n", number, cnumber);
-		if (number == 1)
-		{
-			n--;
-			number++;
-		}
-		else if (number % 2 == 0 && check == false)
-		{
-			cnumber = number;
-			check = true;
-			cnumber /= 2;
-		}
-		else if (number % 3 == 0 && check == false)
-		{
-			cnumber = number;
-			check = true;
-			cnumber /= 3;
-		}
-		else if (number % 5 == 0 && check == false)
-		{
-			cnumber = number;
-			check = true;
-			cnumber /= 5;
-		}
-		else if (cnumber % 2 == 0 && check == true)
-		{
-			cnumber /= 2;
-		}
-		else if (cnumber % 3 == 0 && check == true)
-		{
-			cnumber /= 3;
-		}
-		else if (cnumber % 5 == 0 && check == true)
-		{
-			cnumber /= 5;
-		}
-		else if (cnumber == 1)
-		{
-			n--;
-			number++;
-			check = false;
-		}
-		else
-		{
-			number++;
-			check = false;
-		}
+		
 	}
 	while (n > 0);
 	printf("%d", number);
