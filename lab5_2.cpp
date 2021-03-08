5e4fb8f1cff9aabecda3483e349ef3874780ab5b
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-08 18:46:40 +0700

    thinking a new idea of coding...

--- lab5_2.cpp
+++ lab5_2.cpp
@@ -3,62 +3,6 @@ int main()
 {
 	int n, line = 0;
 	scanf("%d", &n);
-	do
-	{
-		if ((n%2 == 1 && line == (n+1)/2))
-		{
-			printf("*");
-			for (int i = 0; i < n - 3; i++)
-			{
-				printf("-");
-			}
-			printf("*");
-		}
-		else if (n%2 == 0 && line == n/2 || line == n/2-1)
-		{
-			printf("*");
-			for (int i = 0; i < n - 3; i++)
-			{
-				printf("-");
-			}
-			printf("*");
-		}
-		else if (line == 0 || line == n-1)
-		{
-			for (int i = 0; i < (n-1)/2; i++)
-			{
-				printf("-");
-			}
-			printf("*");
-			for (int i = 0; i < (n-1)/2; i++)
-			{
-				printf("-");
-			}
-		}
-		else
-		{
-			if (line <= n/2)
-			{
-				for (int i = 0; i < (n-1)/2-line; i++)
-				{
-					printf("-");
-				}
-				printf("*");
-				for (int i = 0; i < line*2-1; i++)
-				{
-					printf("-");
-				}
-				printf("*");
-				for (int i = 0; i < (n-1)/2-line; i++)
-				{
-					printf("-");
-				}
-			}
-			
-		}
-		printf("\n");
-		line++;
-	} 
-	while (line < n);
+	
 	return 0;
 }
